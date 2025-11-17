6ff8dcef2d0170edaa9efaa8512165af25600236
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-11-17 14:49:13 +0100

    style: spaces

--- front-end/src/pages/NotFound.tsx
+++ front-end/src/pages/NotFound.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
     return (
         <div className="p-8 text-center">
-        <h1 className="text-2xl font-bold">Page introuvable</h1>
-        <p className="text-gray-600">La ressource demandée n’existe pas.</p>
+            <h1 className="text-2xl font-bold">Page introuvable</h1>
+            <p className="text-gray-600">La ressource demandée n’existe pas.</p>
         </div>
     )
 }
