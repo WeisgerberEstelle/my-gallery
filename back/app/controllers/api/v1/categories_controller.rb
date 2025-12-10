1e828ca4682aae5d6fc499c5f7ce2aec55a707de
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-12-10 19:12:08 +0100

    refacto: use blueprinter to serialize categories

--- back/app/controllers/api/v1/categories_controller.rb
+++ back/app/controllers/api/v1/categories_controller.rb
@@ -1,16 +1,10 @@
 module Api
-    module V1
-      class CategoriesController < ApplicationController
-        def index
-          categories = Category.order(:name)
-  
-          render json: categories.map { |category|
-            {
-              id: category.id,
-              name: category.name
-            }
-          }
-        end
+  module V1
+    class CategoriesController < ApplicationController
+      def index
+        categories = Category.order(:name)
+        render json: CategoryBlueprint.render(categories)
       end
     end
+  end
 end
